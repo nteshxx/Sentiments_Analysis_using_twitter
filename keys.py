e6a9137ec3f74562fc5075fb5241a7576d9a58d0
Nitesh Yadav <60145175+nteshxx@users.noreply.github.com>
2021-04-18 19:37:46 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,5 @@
-consumer_key = '9pUVgxx8loAVrKUDnTBVSE5t3'
-consumer_secret = 'Ai3CtrvdPLKTCRU1Ln5CVta4aolVTvXFxgSvXrWrG2miPgVUqv'
-access_token = '1368796376415444994-yKAkItsdNuq5o2dygWAnvC5aBPxk9W'
-access_secret = 'OyYlL9TzSoDsy6puZi4qxN5cTXKPtDDcO52AruqEU8ill'
+#twitter developer account credentials
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
